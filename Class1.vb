7aeea302a26319ebe1294f2812e56ed15e766ae8
Mati F <matiferrero@hotmail.com>
2017-12-10 23:22:40 -0300

    mi primer mensaje

---
+++ Class1.vb
@@ -0,0 +1,16 @@
+﻿Imports Microsoft.VisualBasic
+
+Public Class Class1
+
+
+    Private nom As String
+
+    Function getNombreApellido() As String
+        Return Me.nom
+    End Function
+
+    Sub setNombreApellido(ByVal nombreApellido As String)
+        Me.nom = nombreApellido
+    End Sub
+
+End Class
